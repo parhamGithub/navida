f92e6dbb8935bf703a706d7f0bcd27e062a3e9fe
parham <parhamafshari666@gmail.com>
2026-08-08 21:40:19 +0330

    Fixed the background color. Made it all black

--- app/components/ArchitectureBackground.tsx
+++ app/components/ArchitectureBackground.tsx
@@ -6,7 +6,7 @@ const ArchitectureBackground = memo(function ArchitectureBackground() {
   return (
     <div
       aria-hidden="true"
-      className="pointer-events-none absolute inset-0 -z-10 overflow-hidden bg-[#050505]"
+      className="pointer-events-none absolute inset-0 -z-10 overflow-hidden bg-black"
     >
       {/* Ambient glow */}
       <div
