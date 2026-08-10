a1b83f75e638eacf057bff7efcd3a3ca2b7fe241
parham <parhamafshari666@gmail.com>
2026-08-10 21:15:04 +0330

    refined Hero section padding

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,8 +7,8 @@ import ArchitectureBackground from "@/app/components/ArchitectureBackground";
 export default function Hero() {
   return (
     <section
-      className="relative min-h-screen flex flex-col items-center justify-center text-center px-[6%] pt-40 
-    pb-27.5 overflow-hidden"
+      className="relative min-h-screen flex flex-col items-center justify-center text-center px-[6%] pt-30 
+      pb-27.5 overflow-hidden"
     >
       <ArchitectureBackground />
       <div
